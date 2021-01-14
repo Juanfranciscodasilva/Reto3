fd10d5878f7871d2c01eb94c19b16754b6cbb579
juanfrancisco.dasilva <juanfrancisco.dasilva@ikasle.egibide.org>
2021-01-14 09:53:01 +0100

    Prueba con BBDD heroku

--- resources/views/holaprueba.blade.php
+++ resources/views/holaprueba.blade.php
@@ -4,5 +4,9 @@
 </head>
 <body>
     <h1>Hola has entrado al welcome de prueba creado en views</h1>
+    <form action="random.php">
+        <input type="text">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
