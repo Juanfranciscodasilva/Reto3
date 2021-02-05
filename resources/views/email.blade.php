82b2f3dc9ff98ea0d1091610f1bea64a255351fa
Juanfranciscodasilva <56869952+Juanfranciscodasilva@users.noreply.github.com>
2021-02-05 08:32:51 +0100

    Update email.blade.php

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,9 +1,5 @@
 
 <h2>Email: {{$email}}</h2>
-<h2>Valor: {{$valor}}</h2>
-<<<<<<< HEAD
-<h2>URL: <a href="laravel.test/reset?email={{base64_encode($email)}}" onmouseover="'11hhssjjwj'">enlace de recuperación</a></h2>
-=======
 <h2>URL: <a href="http://prueba-proyecto3.herokuapp.com/reset?email={{base64_encode($email)}}" onmouseover="'11hhssjjwj'">enlace de recuperación</a></h2>
->>>>>>> 38be9f2c63f3f7853629d1682a3c07a64f463954
+
 
